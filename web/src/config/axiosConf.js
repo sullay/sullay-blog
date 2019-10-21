735f3f4a025c17e26572b9208115b7ca49c49e7b
sullay <1181518458@qq.com>
2019-10-21 18:33:53 +0800

    http

--- web/src/config/axiosConf.js
+++ web/src/config/axiosConf.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://127.0.0.1:7000/api'
+const baseUrl = 'http://www.sullay.cn/api'
 // const baseUrl = 'http://192.168.0.139:7000'
 
 axios.defaults.baseURL = baseUrl
--- web/src/config/fileConfig.js
+++ web/src/config/fileConfig.js
@@ -1,6 +1,6 @@
 
-const baseUrl = 'http://127.0.0.1/files/'
-const uploadFileUrl = 'http://127.0.0.1:7000/api/file/uploadFile'
+const baseUrl = 'http://www.sullay.cn/files/'
+const uploadFileUrl = 'http://www.sullay.cn/api/file/uploadFile'
 // const baseUrl = 'http://192.168.0.139/files/'
 // const uploadFileUrl = 'http://192.168.0.139:7000/file/uploadFile'
 export default {
