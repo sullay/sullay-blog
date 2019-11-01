915a71093e1dd027eb86c471c8de02a7fb26016b
sullay <1181518458@qq.com>
2019-11-01 14:06:31 +0800

    ip

--- web/src/config/axiosConf.js
+++ web/src/config/axiosConf.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-// const baseUrl = 'http://www.sullay.cn/api'
-const baseUrl = 'http://192.168.0.111:7000/api'
+const baseUrl = 'http://www.sullay.cn/api'
+// const baseUrl = 'http://192.168.0.111:7000/api'
 
 axios.defaults.baseURL = baseUrl
 export default {
